f5b6bddeec489914eee904115c0348618457e973
Rohanfizz <rohans0501@gmail.com>
2023-01-22 17:54:01 +0530

    bfirst

--- BinarySearch/bsearchFirstOccurance.js
+++ BinarySearch/bsearchFirstOccurance.js
@@ -16,4 +16,19 @@ function firstOccurance(arr,tar){
     }
     return temp;
 }
-console.log(firstOccurance(arr,50));
+function lastOccurance(arr,tar){
+    let n = arr.length;
+    let l = 0;
+    let r = n-1;
+    let temp = -1;
+    while(l<=r){
+        let mid =parseInt((l+r)/2);
+        if(arr[mid] <= tar){
+            if(arr[mid] == tar) temp = mid;
+            l = mid+1;
+        }else r = mid-1;
+    }
+    return temp;
+}
+console.log(firstOccurance(arr,45));
+console.log(lastOccurance(arr,45));
